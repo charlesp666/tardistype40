caaec75ed8d1f4c82cf20fe3220ed6436f6a571b
Charles Pilgrim <charlesp600@msn.com>
2017-10-01 12:25:58 -0700

    Completed adding game time to game and to player stats.

--- LeapFrog/LeapFrog/Player.cs
+++ LeapFrog/LeapFrog/Player.cs
@@ -6,10 +6,7 @@
  * Author:              Charles J Pilgrim
  * Created:             10-October-2013
  * 
- * Last Maintained By:  Charles J Pilgrim
- * Last Maintained:     22-January-2016
- * 
- * @Copyright (c) 2013-2016 Charles J. Pilgrim
+ * @Copyright (c) 2013-2017 Charles J. Pilgrim
  * All Rights Reserved.
  */
 
@@ -37,10 +34,11 @@ namespace LeapFrog
 
         private System.Security.Principal.WindowsIdentity playerID = System.Security.Principal.WindowsIdentity.GetCurrent();
 
-        private int gamesPlayed = 0;                   //Cumulative Number of Games Played by player
-        private int gameWinnings = 0;                               //Cumulative Winnings for player
-        private int countMoves = 0;
+        private int gamesPlayed = 0;                             //Cumulative Number of Games Played
+        private int gameWinnings = 0;                                          //Cumulative Winnings
+        private int countMoves = 0;             //Cumulative Count of Moves Made in All Games Played
         private String namePlayer = "";                         //Name or Identity of Current Player
+        private TimeSpan timePlayed = new TimeSpan(0,0,0);         //Total Time for All Games Played
         
         /*******************************************************************************************
          * Constructor: Player (Default)
@@ -59,7 +57,7 @@ namespace LeapFrog
          * Method: addToMoves
          * Adds the Value passed as Argument to the Current Value of the Object's Game
          * Winnings.
-         * @param aValue - Value (Dollars) to Add to Winnings
+         * @param aValue - Count of Moves in Last Game
          */
         public void addToMoves(int aValue)
         {
@@ -70,7 +68,7 @@ namespace LeapFrog
          * Method: addToWinnings
          * Adds the Value passed as Argument to the Current Value of the Object's Game
          * Winnings.
-         * @param aValue - Value (Dollars) to Add to Winnings
+         * @param aValue - Value to Add to Winnings
          */
         public void addToWinnings(int aValue)
         {
@@ -78,9 +76,37 @@ namespace LeapFrog
         }
 
         /*******************************************************************************************
+          * Method: addToTime
+          * Adds the Game Time to the Cumulative Game Time.
+          * @param aValue - Value (Dollars) to Add to Winnings
+          */
+        public void addToTime(TimeSpan aValue)
+        {
+            this.timePlayed += aValue;
+        }
+
+        /*******************************************************************************************
+          * Method: convertRegistryTimePlayed
+          * Converts the Registry String value of Time Played to a TimeSpan Value
+          */
+        private TimeSpan convertRegistryTimePlayed()
+        {
+            String theRegTime = playerSubKey.GetValue("TimePlayed").ToString();      //Read Registry
+            String[] theTime = theRegTime.Split('.');                          //Remove Milliseconds
+            String[] timeConvert = theTime[0].Split(':');         //Split Hours, Minutes and Seconds
+
+            int theHours = Convert.ToInt32(timeConvert[0]);               //Convert Hours to Integer
+            int theMinutes = Convert.ToInt32(timeConvert[1]);           //Convert Minutes to Integer
+            int theSeconds = Convert.ToInt32(timeConvert[2]);           //Convert Seconds to Integer
+     
+            TimeSpan totalTime = new TimeSpan(theHours, theMinutes, theSeconds); //Build TimeSpan
+
+            return totalTime;
+        }
+
+        /*******************************************************************************************
          * Method: displayPlayerStats
-         * Display the Current Statistics (Name
-         * @param currentScore - Score for Current Game
+         * Display the Current Statistics
          */
         public void displayPlayerStats()
         {
@@ -90,6 +116,9 @@ namespace LeapFrog
             reportScore += "\n\nScore for All Games: " + getGameWinnings();
             reportScore += "\n\nTotal Moves for All Games: " + getCountMoves();
 
+            String totalTimePlayed = convertRegistryTimePlayed().ToString();
+            reportScore += "\n\nTotal Time for All Games: " + totalTimePlayed;
+
             MessageBox.Show(reportScore, messageBoxTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
@@ -98,7 +127,7 @@ namespace LeapFrog
          * Display the Current Statistics (Name
          * @param currentScore - Score for Current Game
          */
-        public void displayPlayerStats(int currentScore, int currentMoves)
+        public void displayPlayerStats(int currentScore, int currentMoves, TimeSpan gameTime)
         {
             String messageBoxTitle = "Player Score";
 
@@ -107,6 +136,11 @@ namespace LeapFrog
             reportScore += "\nMoves in Current Game: " + currentMoves.ToString();
             reportScore += "\n\nScore for All Games: " + getGameWinnings();
             reportScore += "\nTotal Moves for All Games: " + getCountMoves();
+
+            String[] timeOfPlay = gameTime.ToString().Split('.');
+            reportScore += "\n\nGame Time for This Game: " + timeOfPlay[0];
+            String totalTimePlayed = convertRegistryTimePlayed().ToString();
+            reportScore += "\nTotal Time for All Games: " + totalTimePlayed;
 
             MessageBox.Show(reportScore, messageBoxTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
@@ -117,13 +151,14 @@ namespace LeapFrog
          * Registry.
          * @param currentScore - Score for latest game.
          */
-        public void finishGameForPlayer(int currentScore, int numberMoves)
+        public void finishGameForPlayer(int currentScore, int numberMoves, TimeSpan gameTime)
         {
             loadPlayerStats();                                      //Get the Player's current stats
             
             this.incrementGamesPlayed();                 //Increment count of number of games played
             this.addToWinnings(currentScore);                       //Add Winnings to Player's Total
             this.addToMoves(numberMoves);                      //Add Moves to Move Count Accumulator
+            this.addToTime(gameTime);                     //Add Game Time to Time Played Accumulator
             
             writePlayerStats();                          //Write Updated Statistics Back to Registry
         }
@@ -163,7 +198,16 @@ namespace LeapFrog
         {
             return this.namePlayer;
         }
-        
+
+        /*******************************************************************************************
+         * Method: getTimePlayed
+         * Returns the Amount of Time Played
+         */
+        public TimeSpan getTimePlayed()
+        {
+            return this.timePlayed;
+        }
+
         /*******************************************************************************************
          * Method: incrementGamesPlayed
          * Increments the number of games Current Player has played.
@@ -191,6 +235,8 @@ namespace LeapFrog
                 setGamesPlayed((int)playerSubKey.GetValue("GamesPlayed"));
                 setGameWinnings((int)playerSubKey.GetValue("Winnings"));
                 setCountMoves((int)playerSubKey.GetValue("Moves"));
+ 
+                setTimePlayed(convertRegistryTimePlayed());
              }
         }
 
@@ -232,6 +278,15 @@ namespace LeapFrog
         }
 
         /*******************************************************************************************
+         * Method: setTimePlayed
+         * Sets the Total Time Played.
+         */
+        public void setTimePlayed(TimeSpan theTime)
+        {
+            this.timePlayed = theTime;
+        }
+
+        /*******************************************************************************************
          * Method: writePlayerStats
          * Writes Player Stats to the Registry entry for this player's game.
          */
@@ -241,6 +296,7 @@ namespace LeapFrog
             playerSubKey.SetValue("GamesPlayed", this.gamesPlayed);
             playerSubKey.SetValue("Winnings", this.gameWinnings);
             playerSubKey.SetValue("Moves", this.countMoves);
+            playerSubKey.SetValue("TimePlayed", this.timePlayed);
         }
     }
 }
