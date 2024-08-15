cf586a74a48d4876289faf9fcadaa510fd254486
Charles J Pilgrim <charlesp600@msn.com>
2024-08-15 11:26:15 -0700

    Reposition button and text on splash screen.

--- LeapfrogUWP/MainPage.xaml.cs
+++ LeapfrogUWP/MainPage.xaml.cs
@@ -9,7 +9,6 @@
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
